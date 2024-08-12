a1d94e58a9b2ead6cd5371f82a4d7ff57e3b64df
guanyunfang <54895719+guanyunfang@users.noreply.github.com>
2024-08-12 18:14:29 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 yum install -y httpd
+sudo apt-get install wget
+wget ﻿﻿https://dl.google.com/go/go1.21.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
+vim /etc/profile
+
