11d532f1acf8ee0ed2f95e736e3773cdc8ca791a
guanyunfang <54895719+guanyunfang@users.noreply.github.com>
2024-08-12 21:06:57 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,7 +5,7 @@ sudo apt-get install wget
 
 install_path="/usr/local"
 # download golang on studygolang mirror
-wget https://dl.google.com/go/go1.21.5.linux-amd64.tar.gz
+sudo wget https://dl.google.com/go/go1.21.5.linux-amd64.tar.gz
 
 sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
 
