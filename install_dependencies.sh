c38f09e1ce4b85913b5c8ba01b655b69c193c16f
guanyunfang <54895719+guanyunfang@users.noreply.github.com>
2024-08-12 21:27:57 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,8 +12,9 @@ sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
 cd /usr/local
 
 echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee -a /etc/profile
-# echo 'source completed' | sudo source /etc/profile
-echo 'source completed' | . /etc/profile
+bash
+echo 'source completed' | sudo source /etc/profile
+# echo 'source completed' | . /etc/profile
 go version
 
 
